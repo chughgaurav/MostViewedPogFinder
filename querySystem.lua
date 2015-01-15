2543fd5311854c76b739350e3f0ba727ee8ad279
gaurav.chugh <gaurav.chugh@snapdeal.com>
2015-01-16 01:16:09 +0530

    adding sorting fn

--- querySystem.lua
+++ querySystem.lua
@@ -43,9 +43,40 @@ function querySystem(rec)
 		debugMsg = debugMsg.." "..key.."- "..value
 	end
 
-	--for key,value in map.pairs(highestHit) do
-	--	debugMsg = debugMsg.." highestHit key-"..key.." value - "..value
-	--end
+	function spairs(t, order)
+	    -- collect the keys
+	    local keys = {}
+	    for k in pairs(t) do keys[#keys+1] = k end
+
+	    -- if order function given, sort by it by passing the table and keys a, b,
+	    -- otherwise just sort the keys 
+	    if order then
+	        table.sort(keys, function(a,b) return order(t, a, b) end)
+	    else
+	        table.sort(keys)
+	    end
+
+	    -- return the iterator function
+	    local i = 0
+	    return function()
+	        i = i + 1
+	        if keys[i] then
+	            return keys[i], t[keys[i]]
+	        end
+	    end
+	end
+
+	local hitsAvgTb = {}
+	for key,value in map.pairs(hitsAvgMap) do
+		hitsAvgTb[key] = value
+	end
+
+	debugMsg = debugMsg.."-------------sorted--------------"
+
+	for k,v in spairs(hitsAvgTb, function(t,a,b) return t[b] < t[a] end) do
+		debugMsg = debugMsg..k.."  "..v.."   "
+	end
+
 
 	debugMsg = debugMsg.." highestHit key-"..highestHit["key"].." value - "..highestHit["value"]
 
